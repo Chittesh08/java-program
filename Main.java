3d3777400d80f27026abe96ffdd689c849d60ab3
Chittesh08 <chittesh232005@gmail.com>
2025-01-25 14:47:08 +0530

    push

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+   public static void main(String args[]) {
+    int num = 100;
+    System.out.print("Odd Numbers from 1 to "+num+" are: ");
+    for (int i = 1; i <= num; i++) {
+       if (i % 2 != 0) {
+        System.out.print(i + " ");
+       }
+    }
+   }
+}
